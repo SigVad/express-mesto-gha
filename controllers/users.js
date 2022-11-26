89e7e3bde4f5a206868f6858b0560c635ff23180
SigVad <97845386+SigVad@users.noreply.github.com>
2022-11-27 02:11:54 +0300

    fix/ коды ошибок в константах

--- controllers/users.js
+++ controllers/users.js
@@ -1,81 +1,71 @@
 const User = require('../models/user');
 
+const OK_CODE = 200;
+const OK_CREATED_CODE = 201;
+const ERROR_BAD_REQUEST_CODE = 400;
+const ERROR_NOT_FOUND_CODE = 404;
+const ERROR_INTERNAL_SERVER_CODE = 500;
+
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
-      res.status(200).send({ data: users });
+      res.status(OK_CODE).send({ data: users });
     })
     .catch(() => {
-      res.status(500).send({ message: 'Произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER_CODE).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
-    // .orFail(() => {
-    //   throw new Error('Пользователь по указанному _id не найден');
-    // })
     .then((user) => {
-      res.status(200).send({ data: user });
+      res.status(OK_CODE).send({ data: user });
     })
     .catch((err) => {
-      // if (err.name === 'CastError') {
-      //   res.status(400).send({ message: 'Передан некорректный _id' });
-      //   return;
-      // }
-      // if (err.statusCode === 404) {
-      //   res.status(404).send({ message: err.message });
-      //   return;
-      // }
       if (err.name === 'CastError') {
-        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+        res.status(ERROR_NOT_FOUND_CODE).send({ message: 'Пользователь по указанному _id не найден' });
         return;
       }
-      res.status(500).send({ message: 'Произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER_CODE).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
-const postUser = (req, res) => {
+const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
     .then((user) => {
-      res.status(200).send({ data: user });
+      res.status(OK_CREATED_CODE).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({
+        res.status(ERROR_BAD_REQUEST_CODE).send({
           message:
-          'Переданы некорректные данные при создании пользователя.'
+          'Переданы некорректные данные при создании пользователя.',
         });
         return;
       }
-      res.status(500).send({ message: 'Произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER_CODE).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
 const patchUser = (req, res) => {
   const { user: { _id }, body } = req;
   User.findByIdAndUpdate(_id, body, { new: true, runValidators: true })
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(OK_CODE).send({ data: user }))
     .catch((err) => {
-      console.log(err.name);
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+        res.status(ERROR_BAD_REQUEST_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
         return;
       }
       if (err.name === 'CastError') {
-        res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+        res.status(ERROR_NOT_FOUND_CODE).send({ message: 'Пользователь по указанному _id не найден' });
         return;
       }
-      if (err.statusCode === 404) {
-        res.status(404).send({ message: err.message });
-        return;
-      }
-      res.status(500).send({ message: 'Произошла ошибка' });
+      res.status(ERROR_INTERNAL_SERVER_CODE).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
 module.exports = {
-  getUsers, getUserById, postUser, patchUser,
+  getUsers, getUserById, createUser, patchUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const {
-  getUsers, getUserById, postUser, patchUser,
+  getUsers, getUserById, createUser, patchUser,
 } = require('../controllers/users');
 
 router.get('', getUsers); // возвращает всех пользователей
 router.get('/:userId', getUserById); // возвращает пользователя по _id
-router.post('', postUser); // создаёт пользователя
+router.post('', createUser); // создаёт пользователя
 
 router.patch('/me', patchUser); // обновляет профиль
 router.patch('/me/avatar', patchUser); // обновляет аватар
