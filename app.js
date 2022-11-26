6cc300543b2c810f6c4a569679447e30ec7a6a95
SigVad <97845386+SigVad@users.noreply.github.com>
2022-11-27 02:11:23 +0300

    fix/ вернуть прослушивание порта

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-
-// app.listen(PORT, () => {
+app.listen(PORT, () => {
 //   console.log(`App listening on port ${PORT}`);
-// });
+});
