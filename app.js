a672f99c2e649dd735c293bdd951484ec68455e2
SigVad <97845386+SigVad@users.noreply.github.com>
2022-12-08 02:28:22 +0300

    Рефакторинг - облегчить app

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); // Сборка пакетов
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const NotFoundErr = require('./errors/NotFoundErr');
+const routes = require('./routes/routes');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-/* const urlRegExp = new RegExp(
-    '^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w.-]+)+[\\w\\-._~:/?#[\\]@!$&\'()*+,;=.]+$'
-   ); */
 
 // для собирания JSON-формата
 app.use(bodyParser.json());
@@ -32,45 +22,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   autoIndex: true,
 });
 
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().uri().regex(
-        /https?:\/\/(\w{3}\.)?[1-9a-z\-.]{1,}\.\w{2,}(\/[1-90a-z-._~:?#[@!$&'()*+,;=]{1,}\/?)?#?/i,
-      ),
-      email: Joi.string().required().email({ tlds: { allow: false } }),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser,
-);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.use(auth); // защита авторизацией
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundErr('Страница не найдена'));
-});
-
-app.use(errors());
-app.use((err, req, res, next) => { // обработать ошибку сервера
-  const {
-    statusCode = 500,
-    message = 'На сервере произошла ошибка',
-  } = err;
-  res.status(statusCode).send({ message });
-  next();
-});
+app.use(routes); // маршруты
 
 app.listen(PORT);
---
+++ routes/routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { celebrate, Joi, errors } = require('celebrate');
+const usersRouter = require('./users');
+const cardsRouter = require('./cards');
+const { login, createUser } = require('../controllers/users');
+const { urlRegExp } = require('../utils/constants');
+const auth = require('../middlewares/auth');
+const NotFoundErr = require('../errors/NotFoundErr');
+const defaultError = require('../middlewares/defaultError');
+
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().uri().regex(urlRegExp),
+      email: Joi.string().required().email({ tlds: { allow: false } }),
+      password: Joi.string().required(),
+    }),
+  }),
+  createUser,
+);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email({ tlds: { allow: false } }),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.use(auth); // защита авторизацией
+router.use('/', usersRouter);
+router.use('/', cardsRouter);
+
+router.use('*', (req, res, next) => {
+  next(new NotFoundErr('Страница не найдена'));
+});
+
+router.use(errors());
+router.use(defaultError); // обработать ошибку сервера
+
+module.exports = router;
